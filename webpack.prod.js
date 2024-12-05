470697347fa9af1fc9744d5c95a772a157e25646
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-05 21:46:54 +0200

    nav items are done

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,12 +3,4 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
 });

17bd9770b8aa315e90fa4321802416071659d99b
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-06 14:11:14 +0200

    when a button is clicked, it's respective dropdown item comes down

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
 import '../css/styles.css';
+
+//this module will house the drop-down menu
+const dropDownMenu = (function () {
+  //variable declarations of buttons in the navbar(navButton) and it's menu items(megaMenu)
+  const navButtons = document.querySelectorAll('.navLink button');
+  const megaMenu = document.querySelectorAll('.megaMenu');
+  const dropDownState = false;
+
+  navButtons.forEach((navButton, buttonIndex) => {
+    navButtons[buttonIndex].addEventListener('click', () => {
+      megaMenu[buttonIndex].classList.add('visible');
+    });
+  });
+
+  // const dropDownState = navButtons.forEach((navButton, buttonIndex) => {
+  //   navButton.addEventListener('click', (navButton, buttonIndex) => {
+  //     megaMenu.item(buttonIndex).classList.add('visible');
+  //   });
+  // });
+})();
